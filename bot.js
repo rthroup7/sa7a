cb5223d0fc1282198f7dd231962571c1106a0325
rthroup7 <45805083+rthroup7@users.noreply.github.com>
2018-12-24 09:02:50 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,17 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم 
+لو سمحت ممكن تدخل سيرفرى
+                               [https://discord.gg/bT3hGJB] 
+الدعووة خاصة لك ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+client.login(process.env.TOKEN);
